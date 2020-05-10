b31aff2c7c8720e5b20d49a1710c53b197a02920
Eddy Ash <eddyashimov@gmail.com>
2020-05-09 21:44:38 -0500

    Initial Commit

---
+++ Project 25 (Multipeering and collectionview, gcd)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 25 (Multipeering and collectionview, gcd)
+//
+//  Created by Edil Ashimov on 5/9/20.
+//  Copyright © 2020 Edil Ashimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
